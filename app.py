e9004cc041310d4108437004a9612e0a07ff1b66
Satyam Gupta <127432978+Satyaemsec@users.noreply.github.com>
2023-03-17 11:32:14 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+"""An Nmap Scanner and Parser."""
+
+import xmltodict
+from subprocess import run
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Return the main page."""
+    return "Nmap Scanner, a more sofisticated page is coming soon."
+
+
+@app.route('/openports')
+def scan_localhost():
+    """Scan a host with Nmap."""
+    # run nmap scan from another proccess
+    run(['nmap', '-T5', '--open', '-oX', 'scan.xml', 'nokia.com'])
+
+    # parse Nmap XML report and covert it to a dictionary
+    with open('scan.xml') as raw_xml:
+        nmap_scan = xmltodict.parse(raw_xml.read())
+
+    # Jsonify the dictionary and return it
+    return jsonify(nmap_scan)
+
+
+if __name__ == '__main__':
+    app.run()
